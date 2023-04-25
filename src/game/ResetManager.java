336eaf5943305ff700304074d1a5a726ff1dea5e
Hayden Tran <htra0059@student.monash.edu>
2023-04-26 00:27:34 +1000

    implemented the provided methods of ResetManager

--- src/game/ResetManager.java
+++ src/game/ResetManager.java
@@ -18,13 +18,43 @@ public class ResetManager {
      * HINT 1: where have we seen a private constructor before?
      * HINT 2: see the instance attribute above.
      */
+
+    /**
+     * gets the instance of reset manager
+     */
     private ResetManager() {
         this.resettables = new ArrayList<>();
     }
 
-    public void run() {}
+    public static ResetManager getInstance(){
+        if (instance == null) {
+            instance = new ResetManager();
+        }
+        return instance;
+    }
 
-    public void registerResettable(Resettable resettable) {}
+    /**
+     * resets game by going through all resettable objects in the list
+     */
+    public void run() {
+        for (Resettable object : this.resettables){
+            object.reset();
+        }
+    }
 
-    public void removeResettable(Resettable resettable) {}
+    /**
+     * adds resettable instance to the list
+     * @param resettable
+     */
+    public void registerResettable(Resettable resettable) {
+        this.resettables.add(resettable);
+    }
+
+    /**
+     * remove resettable instance from the list
+     * @param resettable
+     */
+    public void removeResettable(Resettable resettable) {
+        this.resettables.remove(resettable);
+    }
 }
