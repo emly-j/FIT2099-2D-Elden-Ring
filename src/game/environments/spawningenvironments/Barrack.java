e8e6a8b501cff122414851cb63ede9675b09fd10
emil0009 <emil0009@student.monash.edu>
2023-05-09 15:54:15 +1000

    Added Barrack and Cage classes

---
+++ src/game/environments/spawningenvironments/Barrack.java
@@ -0,0 +1,31 @@
+package game.environments.spawningenvironments;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.enemies.soldiers.GodrickSoldier;
+import game.utils.RandomNumberGenerator;
+
+public class Barrack extends SpawningGround {
+    /**
+     * Constructor.
+     */
+    public Barrack() {
+        super('B');
+        this.addActorThatSpawns(new GodrickSoldier(), 45);
+    }
+
+    /**
+     * A factory method to spawn actors depending on their chance of spawning and the location on the map.
+     *
+     * @param actor    actor that may potentially spawn
+     * @param location location where the actor will spawn
+     */
+    @Override
+    public void spawnActor(Actor actor, Location location) {
+        boolean canSpawn = RandomNumberGenerator.getRandomChance(getActorSpawnChance(actor));
+
+        if(canSpawn){
+            location.addActor(actor);
+        }
+    }
+}
